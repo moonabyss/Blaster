ec7f8a168ab856d6cd8080c78d64bb9cebec5d82
moonabyss <d-v@ukr.net>
2023-07-22 23:13:06 +0300

    05.12. Shell Physics

--- Source/Blaster/Private/Weapon/BlasterWeaponShell.cpp
+++ Source/Blaster/Private/Weapon/BlasterWeaponShell.cpp
@@ -1,6 +1,8 @@
 // Blaster Multiplayer Game. All rights reserved.
 
 #include "Weapon/BlasterWeaponShell.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundBase.h"
 
 ABlasterWeaponShell::ABlasterWeaponShell()
 {
@@ -8,9 +10,27 @@ ABlasterWeaponShell::ABlasterWeaponShell()
 
     ShellMesh = CreateDefaultSubobject<UStaticMeshComponent>("ShellMesh");
     SetRootComponent(ShellMesh);
+    ShellMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+    ShellMesh->SetSimulatePhysics(true);
+    ShellMesh->SetEnableGravity(true);
+    ShellMesh->SetNotifyRigidBodyCollision(true);
 }
 
 void ABlasterWeaponShell::BeginPlay()
 {
     Super::BeginPlay();
+
+    ShellMesh->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+
+    ShellMesh->AddImpulse(GetActorRightVector() * ShellEjectionImpulse);
+    SetLifeSpan(LifeSpanTime);
+}
+
+void ABlasterWeaponShell::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    if (ShellSound)
+    {
+        ShellMesh->OnComponentHit.RemoveAll(this);
+        UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+    }
 }
--- Source/Blaster/Public/Weapon/BlasterWeaponShell.h
+++ Source/Blaster/Public/Weapon/BlasterWeaponShell.h
@@ -7,6 +7,8 @@
 
 #include "BlasterWeaponShell.generated.h"
 
+class USoundBase;
+
 UCLASS()
 class BLASTER_API ABlasterWeaponShell : public AActor
 {
@@ -17,8 +19,19 @@ public:
 
 protected:
     virtual void BeginPlay() override;
+    UFUNCTION()
+    virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
     UPROPERTY(Category = "Components", VisibleAnywhere)
     UStaticMeshComponent* ShellMesh;
+
+    UPROPERTY(Category = "Weapon Shell", EditDefaultsOnly)
+    float ShellEjectionImpulse{10.0f};
+
+    UPROPERTY(Category = "Weapon Shell", EditDefaultsOnly)
+    float LifeSpanTime{1.0f};
+
+    UPROPERTY(Category = "Weapon Shell", EditDefaultsOnly)
+    USoundBase* ShellSound;
 };
